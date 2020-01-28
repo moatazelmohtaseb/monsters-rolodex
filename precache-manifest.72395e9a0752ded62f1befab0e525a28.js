8eb45ad1e790de0e68cd3da8a58910cface501b0
Moataz <moataz.ali1@vodafone.com>
2020-01-28 14:01:03 +0200

    Updates

---
+++ precache-manifest.72395e9a0752ded62f1befab0e525a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03f807b5f8ae0538fe5335d55aab401",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b041eefefda7bdea980",
+    "url": "/monsters-rolodex/static/css/main.2ebc2f91.chunk.css"
+  },
+  {
+    "revision": "500481e5bfd2ffec9bca",
+    "url": "/monsters-rolodex/static/js/2.958cebbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.958cebbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b041eefefda7bdea980",
+    "url": "/monsters-rolodex/static/js/main.2e5b7337.chunk.js"
+  },
+  {
+    "revision": "c816b0b208217d787bf4",
+    "url": "/monsters-rolodex/static/js/runtime-main.502a3996.js"
+  }
+]);
